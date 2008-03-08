f748bf4030f4b22e6051ecc538fff405a09efcf3
sema <sema@cb2309eb-351f-4c53-ad2e-ab06ce95a7ee>
2008-03-08 18:57:15 +0000

    Forkortet tekst i medlemsskabs tabellen for at spare lidt plads.

--- kitasite/core/templatetags/core_tags.py
+++ kitasite/core/templatetags/core_tags.py
@@ -74,7 +74,17 @@ def translate(text, category):
                         return _('Passive member')
                 elif text == MembershipState.INACTIVE:
                         return _('Inactive member')
-        
+                
+        if category == "membership_state_short":
+                if text == MembershipState.ACTIVE:
+                        return _('Active')
+                elif text == MembershipState.CONDITIONAL_ACTIVE:
+                        return _('Conditional active')
+                elif text == MembershipState.PASSIVE:
+                        return _('Passive')
+                elif text == MembershipState.INACTIVE:
+                        return _('Inactive')
+                      
         elif category == "payment_type":
                 if text == "FULL":
                         return _('Full payment')
