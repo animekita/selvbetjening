b7455fa4169972510f946af71eef501fb9471730
sema <sema@cb2309eb-351f-4c53-ad2e-ab06ce95a7ee>
2008-08-15 18:38:12 +0000

    Rettet fejl ved visning af tilvalg efter den seneste opdatering.

--- kitasite/eventmode/tests.py
+++ kitasite/eventmode/tests.py
@@ -13,33 +13,33 @@ class DatabaseSetup(object):
     def setUp(target):
         day = timedelta(days=1)
         today = date.today()
-        
+
         target.event = Event.objects.create(title='event1', registration_open=True,
                                         enddate=today, startdate=today-day)
-        
+
         target.event2 = Event.objects.create(title='event1', registration_open=True,
-                                        enddate=today-day, startdate=today-day*2)  
-        
+                                        enddate=today-day, startdate=today-day*2)
+
         target.eventmode1 = EventmodeModel.objects.create(passphrase='ww',
                                                         event=target.event)
-        
+
         target.eventmode2 = EventmodeModel.objects.create(passphrase='www',
-                                                        event=target.event2)  
+                                                        event=target.event2)
     setUp = staticmethod(setUp)
 
 class EventmodeModelTestCase(TestCase):
     def setUp(self):
         DatabaseSetup.setUp(self)
-    
+
     def test_is_valid(self):
         self.assertTrue(self.eventmode1.is_valid())
         self.assertFalse(self.eventmode2.is_valid())
-        
+
     def test_check_passphrase(self):
         self.assertTrue(EventmodeModel.objects.check_passphrase('ww'))
-    
+
     def test_check_passphrase_wrong(self):
-        self.assertFalse(EventmodeModel.objects.check_passphrase('ww_random_part'))     
+        self.assertFalse(EventmodeModel.objects.check_passphrase('ww_random_part'))
 
 class EventmodeTestCase(TestCase):
     def setUp(self):
@@ -47,37 +47,37 @@ class EventmodeTestCase(TestCase):
 
         class dummyRequest():
             session = {}
-        
+
         self.dummyRequest = dummyRequest()
         self.eventmode = Eventmode(self.dummyRequest)
 
     def test_activate(self):
         self.assertTrue(self.eventmode.activate('ww'))
-    
+
     def test_activate_old(self):
         self.assertFalse(self.eventmode.activate('www'))
-    
+
     def test_activate_nonexisting(self):
         self.assertFalse(self.eventmode.activate('ww_random_part'))
-        
+
     def test_is_not_active(self):
         self.assertFalse(self.eventmode.is_active())
 
     def test_is_active(self):
         self.eventmode.activate('ww')
-        self.assertTrue(self.eventmode.is_active())   
+        self.assertTrue(self.eventmode.is_active())
 
     def test_is_active_old(self):
         self.eventmode.activate('www')
-        self.assertFalse(self.eventmode.is_active())          
-        
+        self.assertFalse(self.eventmode.is_active())
+
     def test_deactivate(self):
         self.test_is_active()
         self.assertTrue(self.eventmode.is_active())
-        
+
         self.eventmode.deactivate()
         self.assertFalse(self.eventmode.is_active())
-    
+
     def test_get_model(self):
         self.test_is_active()
         self.assertEqual(self.eventmode.get_model(), self.eventmode1)
--- kitasite/eventmode/views.py
+++ kitasite/eventmode/views.py
@@ -23,9 +23,9 @@ from core.decorators import log_access
 @permission_required('events.change_attend')
 @log_access
 def event_checkin(request, event_id, template_name='eventmode/checkin.html'):
-    
+
     event = shortcuts.get_object_or_404(Event, id=event_id)
-    
+
     return render_to_response(template_name,
                               {'attendees' : event.get_attendees(), 'event' : event},
                               context_instance=RequestContext(request))
@@ -33,15 +33,15 @@ def event_checkin(request, event_id, template_name='eventmode/checkin.html'):
 @permission_required('events.change_attend')
 @log_access
 def event_usercheckin(request, event_id, user_id, template_name='eventmode/usercheckin.html'):
-    
+
     attend = shortcuts.get_object_or_404(Attend, event=event_id, user=user_id)
-    
+
     membershipState = attend.user.get_profile().get_membership_state()
-    
+
     if membershipState == MembershipState.CONDITIONAL_ACTIVE:
         if attend.user.get_profile().member_since().date() == datetime.today().date():
             membershipState = MembershipState.ACTIVE
-    
+
     if membershipState == MembershipState.INACTIVE or membershipState == MembershipState.PASSIVE or membershipState == MembershipState.CONDITIONAL_ACTIVE:
         needsToPay = True
         if request.method == 'POST':
@@ -61,7 +61,7 @@ def event_usercheckin(request, event_id, user_id, template_name='eventmode/userc
                 return HttpResponseRedirect(reverse('eventmode_checkin', kwargs={'event_id':event_id}))
         else:
             form = CheckinForm()
-    
+
     return render_to_response(template_name,
                               {'user' : attend.user, 'event' : attend.event, 'attend' : attend,
                                'form' : form, 'needs_to_pay' : needsToPay},
@@ -70,24 +70,24 @@ def event_usercheckin(request, event_id, user_id, template_name='eventmode/userc
 @permission_required('events.change_attend')
 @log_access
 def event_options(request, event_id, template_name='eventmode/options.html'):
-    
+
     event = get_object_or_404(Event, id=event_id)
-    
+
     return render_to_response(template_name,
-                              {'event' : event, 'options' : event.option_set.all()},
+                              {'event' : event, 'optiongroups' : event.optiongroup_set.all()},
                               context_instance=RequestContext(request))
 
 @permission_required('events.change_attend')
 @log_access
-def event_options_detail(request, event_id, option_id, 
+def event_options_detail(request, event_id, option_id,
                              template_name='eventmode/options_detail.html'):
-    
+
     option = get_object_or_404(Option, id=option_id)
-    
+
     return render_to_response(template_name,
-                              {'event' : option.event, 'option' : option, 
+                              {'event' : option.group.event, 'option' : option,
                                'users' : option.users.all()},
-                              context_instance=RequestContext(request))    
+                              context_instance=RequestContext(request))
 
 @permission_required('events.change_attend')
 @log_access
@@ -96,7 +96,7 @@ def event_list(request, template_name='eventmode/list.html'):
                               context_instance=RequestContext(request))
 
 def activate_mode(request, template_name='eventmode/activate_mode.html',
-                  form_class=EventmodeAccessForm, success_page='home'):  
+                  form_class=EventmodeAccessForm, success_page='home'):
 
     if request.method == 'POST':
         form = form_class(request.POST)
@@ -107,16 +107,16 @@ def activate_mode(request, template_name='eventmode/activate_mode.html',
             return HttpResponseRedirect(reverse(success_page))
     else:
         form = form_class()
-    
+
     return render_to_response(template_name,
                               {'form' : form},
                               context_instance=RequestContext(request))
 
-def deactivate_mode(request):  
+def deactivate_mode(request):
     request.eventmode.deactivate()
-    
+
     return HttpResponseRedirect(reverse('home'))
 
 def info(request, template_name='eventmode/info.html'):
     return render_to_response(template_name, {},
-                              context_instance=RequestContext(request))   
+                              context_instance=RequestContext(request))
