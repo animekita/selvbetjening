e037a09baa25db8b16a9c7fa70a93aac2284f8ab
sema <sema@cb2309eb-351f-4c53-ad2e-ab06ce95a7ee>
2009-11-22 16:24:25 +0000

    Forbedret understøttelse af flere sprog i Selvbetjening.

--- selvbetjening/data/events/forms.py
+++ selvbetjening/data/events/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 class OptionGroupForm(forms.Form):
     def __init__(self, optiongroup, *args,  **kwargs):
--- selvbetjening/data/members/forms.py
+++ selvbetjening/data/members/forms.py
@@ -2,7 +2,7 @@
 import re
 
 from django import forms
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
 
 from models import UserProfile
