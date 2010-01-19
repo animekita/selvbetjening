4faf89e2a013f721119a2665db526823933ed133
sema <sema@cb2309eb-351f-4c53-ad2e-ab06ce95a7ee>
2010-01-19 19:39:54 +0000

    Rettet fejl i migration.

--- selvbetjening/data/events/migrations/0015_addstate_step1.py
+++ selvbetjening/data/events/migrations/0015_addstate_step1.py
@@ -5,179 +5,23 @@ from django.db import models
 from selvbetjening.data.events.models import *
 
 class Migration:
-    
+
     def forwards(self, orm):
-        
+
         # Adding field 'Attend.state'
         db.add_column('events_attend', 'state', orm['events.attend:state'])
-        
+
         # Adding field 'Event.move_to_accepted_policy'
         db.add_column('events_event', 'move_to_accepted_policy', orm['events.event:move_to_accepted_policy'])
-        
-        # Changing field 'Event.startdate'
-        # (to signature: django.db.models.fields.DateField(null=True, blank=True))
-        db.alter_column('events_event', 'startdate', orm['events.event:startdate'])
-        
-        # Changing field 'Event.enddate'
-        # (to signature: django.db.models.fields.DateField(null=True, blank=True))
-        db.alter_column('events_event', 'enddate', orm['events.event:enddate'])
-        
-        # Changing field 'Event.title'
-        # (to signature: django.db.models.fields.CharField(max_length=255))
-        db.alter_column('events_event', 'title', orm['events.event:title'])
-        
-        # Changing field 'Event.show_change_confirmation'
-        # (to signature: django.db.models.fields.BooleanField(blank=True))
-        db.alter_column('events_event', 'show_change_confirmation', orm['events.event:show_change_confirmation'])
-        
-        # Changing field 'Event.registration_open'
-        # (to signature: django.db.models.fields.BooleanField(blank=True))
-        db.alter_column('events_event', 'registration_open', orm['events.event:registration_open'])
-        
-        # Changing field 'Event.show_registration_confirmation'
-        # (to signature: django.db.models.fields.BooleanField(blank=True))
-        db.alter_column('events_event', 'show_registration_confirmation', orm['events.event:show_registration_confirmation'])
-        
-        # Changing field 'Event.maximum_attendees'
-        # (to signature: django.db.models.fields.IntegerField())
-        db.alter_column('events_event', 'maximum_attendees', orm['events.event:maximum_attendees'])
-        
-        # Changing field 'Event.show_invoice_page'
-        # (to signature: django.db.models.fields.BooleanField(blank=True))
-        db.alter_column('events_event', 'show_invoice_page', orm['events.event:show_invoice_page'])
-        
-        # Changing field 'Attend.has_attended'
-        # (to signature: django.db.models.fields.BooleanField(blank=True))
-        db.alter_column('events_attend', 'has_attended', orm['events.attend:has_attended'])
-        
-        # Changing field 'OptionGroup.freeze_time'
-        # (to signature: django.db.models.fields.DateTimeField(null=True, blank=True))
-        db.alter_column('events_optiongroup', 'freeze_time', orm['events.optiongroup:freeze_time'])
-        
-        # Changing field 'OptionGroup.minimum_selected'
-        # (to signature: django.db.models.fields.IntegerField())
-        db.alter_column('events_optiongroup', 'minimum_selected', orm['events.optiongroup:minimum_selected'])
-        
-        # Changing field 'OptionGroup.maximum_selected'
-        # (to signature: django.db.models.fields.IntegerField())
-        db.alter_column('events_optiongroup', 'maximum_selected', orm['events.optiongroup:maximum_selected'])
-        
-        # Changing field 'OptionGroup.maximum_attendees'
-        # (to signature: django.db.models.fields.IntegerField())
-        db.alter_column('events_optiongroup', 'maximum_attendees', orm['events.optiongroup:maximum_attendees'])
-        
-        # Changing field 'OptionGroup.order'
-        # (to signature: django.db.models.fields.IntegerField())
-        db.alter_column('events_optiongroup', 'order', orm['events.optiongroup:order'])
-        
-        # Changing field 'OptionGroup.name'
-        # (to signature: django.db.models.fields.CharField(max_length=255))
-        db.alter_column('events_optiongroup', 'name', orm['events.optiongroup:name'])
-        
-        # Changing field 'Option.maximum_attendees'
-        # (to signature: django.db.models.fields.IntegerField(null=True, blank=True))
-        db.alter_column('events_option', 'maximum_attendees', orm['events.option:maximum_attendees'])
-        
-        # Changing field 'Option.freeze_time'
-        # (to signature: django.db.models.fields.DateTimeField(null=True, blank=True))
-        db.alter_column('events_option', 'freeze_time', orm['events.option:freeze_time'])
-        
-        # Changing field 'Option.order'
-        # (to signature: django.db.models.fields.IntegerField())
-        db.alter_column('events_option', 'order', orm['events.option:order'])
-        
-        # Changing field 'Option.name'
-        # (to signature: django.db.models.fields.CharField(max_length=255))
-        db.alter_column('events_option', 'name', orm['events.option:name'])
-        
-    
-    
+
     def backwards(self, orm):
-        
+
         # Deleting field 'Attend.state'
         db.delete_column('events_attend', 'state')
-        
+
         # Deleting field 'Event.move_to_accepted_policy'
         db.delete_column('events_event', 'move_to_accepted_policy')
-        
-        # Changing field 'Event.startdate'
-        # (to signature: models.DateField(_(u'start date'), null=True, blank=True))
-        db.alter_column('events_event', 'startdate', orm['events.event:startdate'])
-        
-        # Changing field 'Event.enddate'
-        # (to signature: models.DateField(_(u'end date'), null=True, blank=True))
-        db.alter_column('events_event', 'enddate', orm['events.event:enddate'])
-        
-        # Changing field 'Event.title'
-        # (to signature: models.CharField(_(u'title'), max_length=255))
-        db.alter_column('events_event', 'title', orm['events.event:title'])
-        
-        # Changing field 'Event.show_change_confirmation'
-        # (to signature: models.BooleanField())
-        db.alter_column('events_event', 'show_change_confirmation', orm['events.event:show_change_confirmation'])
-        
-        # Changing field 'Event.registration_open'
-        # (to signature: models.BooleanField(_(u'registration open')))
-        db.alter_column('events_event', 'registration_open', orm['events.event:registration_open'])
-        
-        # Changing field 'Event.show_registration_confirmation'
-        # (to signature: models.BooleanField())
-        db.alter_column('events_event', 'show_registration_confirmation', orm['events.event:show_registration_confirmation'])
-        
-        # Changing field 'Event.maximum_attendees'
-        # (to signature: models.IntegerField(_('Maximum attendees')))
-        db.alter_column('events_event', 'maximum_attendees', orm['events.event:maximum_attendees'])
-        
-        # Changing field 'Event.show_invoice_page'
-        # (to signature: models.BooleanField())
-        db.alter_column('events_event', 'show_invoice_page', orm['events.event:show_invoice_page'])
-        
-        # Changing field 'Attend.has_attended'
-        # (to signature: models.BooleanField())
-        db.alter_column('events_attend', 'has_attended', orm['events.attend:has_attended'])
-        
-        # Changing field 'OptionGroup.freeze_time'
-        # (to signature: models.DateTimeField(_('Freeze time'), null=True, blank=True))
-        db.alter_column('events_optiongroup', 'freeze_time', orm['events.optiongroup:freeze_time'])
-        
-        # Changing field 'OptionGroup.minimum_selected'
-        # (to signature: models.IntegerField(_('Minimum selected')))
-        db.alter_column('events_optiongroup', 'minimum_selected', orm['events.optiongroup:minimum_selected'])
-        
-        # Changing field 'OptionGroup.maximum_selected'
-        # (to signature: models.IntegerField(_('Maximum selected')))
-        db.alter_column('events_optiongroup', 'maximum_selected', orm['events.optiongroup:maximum_selected'])
-        
-        # Changing field 'OptionGroup.maximum_attendees'
-        # (to signature: models.IntegerField(_('Maximum attendees')))
-        db.alter_column('events_optiongroup', 'maximum_attendees', orm['events.optiongroup:maximum_attendees'])
-        
-        # Changing field 'OptionGroup.order'
-        # (to signature: models.IntegerField(_('Order')))
-        db.alter_column('events_optiongroup', 'order', orm['events.optiongroup:order'])
-        
-        # Changing field 'OptionGroup.name'
-        # (to signature: models.CharField(_('Name'), max_length=255))
-        db.alter_column('events_optiongroup', 'name', orm['events.optiongroup:name'])
-        
-        # Changing field 'Option.maximum_attendees'
-        # (to signature: models.IntegerField(_('Maximum attendees'), null=True, blank=True))
-        db.alter_column('events_option', 'maximum_attendees', orm['events.option:maximum_attendees'])
-        
-        # Changing field 'Option.freeze_time'
-        # (to signature: models.DateTimeField(_('Freeze time'), null=True, blank=True))
-        db.alter_column('events_option', 'freeze_time', orm['events.option:freeze_time'])
-        
-        # Changing field 'Option.order'
-        # (to signature: models.IntegerField(_('Order')))
-        db.alter_column('events_option', 'order', orm['events.option:order'])
-        
-        # Changing field 'Option.name'
-        # (to signature: models.CharField(_('Name'), max_length=255))
-        db.alter_column('events_option', 'name', orm['events.option:name'])
-        
-    
-    
+
     models = {
         'auth.group': {
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
@@ -277,5 +121,5 @@ class Migration:
             'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
         }
     }
-    
+
     complete_apps = ['events']
