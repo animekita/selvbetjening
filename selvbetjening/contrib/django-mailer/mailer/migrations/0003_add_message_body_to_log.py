97b08790cbe1464c625d15aa72dddb56a4329635
sema <sema@cb2309eb-351f-4c53-ad2e-ab06ce95a7ee>
2010-02-01 19:21:52 +0000

    Tilføjet manglende fil.

---
+++ selvbetjening/contrib/django-mailer/mailer/migrations/0003_add_message_body_to_log.py
@@ -0,0 +1,53 @@
+
+from south.db import db
+from django.db import models
+from mailer.models import *
+
+class Migration:
+    
+    def forwards(self, orm):
+        
+        # Adding field 'MessageLog.message_body_html'
+        db.add_column('mailer_messagelog', 'message_body_html', orm['mailer.messagelog:message_body_html'])
+        
+    
+    
+    def backwards(self, orm):
+        
+        # Deleting field 'MessageLog.message_body_html'
+        db.delete_column('mailer_messagelog', 'message_body_html')
+        
+    
+    
+    models = {
+        'mailer.dontsendentry': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'to_address': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'when_added': ('django.db.models.fields.DateTimeField', [], {})
+        },
+        'mailer.message': {
+            'from_address': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'message_body': ('django.db.models.fields.TextField', [], {}),
+            'message_body_html': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'priority': ('django.db.models.fields.CharField', [], {'default': "'2'", 'max_length': '1'}),
+            'subject': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'to_address': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'when_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'})
+        },
+        'mailer.messagelog': {
+            'from_address': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'log_message': ('django.db.models.fields.TextField', [], {}),
+            'message_body': ('django.db.models.fields.TextField', [], {}),
+            'message_body_html': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'priority': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'result': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'subject': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'to_address': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'when_added': ('django.db.models.fields.DateTimeField', [], {}),
+            'when_attempted': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'})
+        }
+    }
+    
+    complete_apps = ['mailer']
