44d93b1568e6486b863b874cb7dd653512e01501
Casper S. Jensen <casper@svenningjensen.dk>
2012-07-14 19:09:05 -0700

    Add user count to group sadmin page

--- selvbetjening/sadmin/members/admins/group.py
+++ selvbetjening/sadmin/members/admins/group.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import Group
 
 from selvbetjening.sadmin.base.sadmin import SModelAdmin
 
+def _user_count(group):
+    return group.user_set.all().count()
+_user_count.short_description = _('User count')
+
 class GroupAdmin(SModelAdmin):
     class Meta:
         app_name = 'members'
@@ -14,6 +18,7 @@ class GroupAdmin(SModelAdmin):
     search_fields = ('name',)
     ordering = ('name',)
     filter_horizontal = ('permissions',)
+    list_display = ('name', _user_count)
 
     def changelist_view(self, request, extra_context=None):
         extra_context = extra_context or {}
