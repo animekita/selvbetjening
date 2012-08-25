987f987cc0932be4dfcbdd9fa7657fe039df0253
Casper S. Jensen <casper@svenningjensen.dk>
2012-08-24 17:35:54 -0700

    Remove setup.py again

--- setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
-
-setup(name='Selvbetjening',
-      version='7.2.0',
-      url='http://github.com/kita/selvbetjening/',
-      packages=['selvbetjening']
-)
