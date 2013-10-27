4b3b62dd437f3757efb8899ae966a157e4eeb412
Casper Svenning Jensen <casper@svenningjensen.dk>
2013-10-27 16:08:15 +0100

    Add missing dependency in migration

--- selvbetjening/core/user/migrations/0002_copy_users.py
+++ selvbetjening/core/user/migrations/0002_copy_users.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(DataMigration):
 
+    depends_on = (
+        ("members", "0006_inline_communication_data"),
+    )
+
     def forwards(self, orm):
 
         for old_u in orm['auth.User'].objects.all():
