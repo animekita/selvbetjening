3a2311770e708b9969b522c68515e23d6e92c542
Casper Svenning Jensen <casper@svenningjensen.dk>
2013-11-30 11:49:14 +0100

    Wrong usage of user class

--- selvbetjening/sadmin2/views/autocomplete.py
+++ selvbetjening/sadmin2/views/autocomplete.py
@@ -1,6 +1,5 @@
 
-from django.contrib.auth.models import User
-
+from selvbetjening.core.members.models import SUser
 from selvbetjening.core.events.models import Attend
 from selvbetjening.sadmin2.decorators import sadmin_prerequisites
 from selvbetjening.sadmin2.views.generic import search_view
@@ -9,7 +8,7 @@ from selvbetjening.sadmin2.views.generic import search_view
 @sadmin_prerequisites
 def users(request):
 
-    queryset = User.objects.all()
+    queryset = SUser.objects.all()
     columns = ('username', 'first_name', 'last_name', 'email')
 
     return search_view(request,
